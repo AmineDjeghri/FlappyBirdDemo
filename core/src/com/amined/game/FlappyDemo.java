918dc98ba3bb5bace2f7697c047a588168dfd7e9
Amine Djeghri <32715913+AmineDjeghri@users.noreply.github.com>
2018-07-29 03:44:58 +0200

    Update FlappyDemo.java

--- core/src/com/amined/game/FlappyDemo.java
+++ core/src/com/amined/game/FlappyDemo.java
@@ -4,6 +4,7 @@ import com.amined.game.states.GameStateManager;
 import com.amined.game.states.MenuState;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -16,11 +17,17 @@ public class FlappyDemo extends ApplicationAdapter {
  
         private GameStateManager gsm;       
         private SpriteBatch batch;
+        
+        private Music music;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
                 gsm=new GameStateManager();
+                music=Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+                music.setLooping(true);
+                music.setVolume(0.1f);
+                music.play();
                 Gdx.gl.glClearColor(1, 0, 0, 1);
                 gsm.push(new MenuState(gsm));
                 
@@ -34,6 +41,12 @@ public class FlappyDemo extends ApplicationAdapter {
                 gsm.update(Gdx.graphics.getDeltaTime());
                 gsm.render(batch);
 	}
-	
 
+    @Override
+    public void dispose() {
+        super.dispose(); //To change body of generated methods, choose Tools | Templates.
+        music.dispose();
+    }
+	
+        
 }
